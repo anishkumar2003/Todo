dc6c448eb54c091c5ef5c43cf3e76288b7cc9c1a
Anish Kumar <kumaranish200a@gmail.com>
2024-03-17 11:57:11 +0530

    Border update

--- src/Componenets/Todoformui/TodoForm.jsx
+++ src/Componenets/Todoformui/TodoForm.jsx
@@ -23,25 +23,25 @@ function TodoForm() {
       <input
         type="text"
         placeholder="Write Todo..."
-        className="w-full border border-black/10 rounded-l-lg px-3 outline-none duration-150 bg-white/20 py-1.5"
+        className="w-full border border-black/10 rounded-lg px-3 outline-none duration-150 bg-white/20 py-1.5"
         value={todo}
         onChange={(e) => setTodo(e.target.value)}
       />
       <input
         type="date"
-        className="w-full border border-black/10 rounded-l-lg px-3 outline-none duration-150 bg-white/20 py-1.5"
+        className="w-full border border-black/10 rounded-lg px-3 outline-none duration-150 bg-white/20 py-1.5"
         value={date}
         onChange={(e) => setDate(e.target.value)}
       />
       <input
         type="time"
-        className="w-full border border-black/10 rounded-l-lg px-3 outline-none duration-150 bg-white/20 py-1.5"
+        className="w-full border border-black/10 rounded-lg px-3 outline-none duration-150 bg-white/20 py-1.5"
         value={time}
         onChange={(e) => setTime(e.target.value)}
       />
       <button
         type="submit"
-        className="rounded-r-lg px-3 py-1 bg-green-600 text-white shrink-0"
+        className="rounded-lg px-3 py-1 bg-green-600 text-white shrink-0"
       >
         Add
       </button>
