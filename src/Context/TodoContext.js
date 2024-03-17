6d4b7cd536970c7b01e59a7494cfd2f38cf66ec2
Anish Kumar <kumaranish200a@gmail.com>
2024-03-17 17:24:01 +0530

    remove unwanted code

--- src/Context/TodoContext.js
+++ src/Context/TodoContext.js
@@ -1,22 +1,6 @@
 import { createContext, useContext } from "react";
 export const TodoContext = createContext({
-  todos: [
-    {
-      id: 1,
-      msg: "Learn React",
-      completed: false,
-    },
-    {
-      id: 2,
-      msg: "Learn Tailwind",
-      completed: false,
-    },
-    {
-      id: 3,
-      msg: "Learn Firebase",
-      completed: false,
-    },
-  ],
+  todos: [],
   addTodo: (todo) => {},
   deleteTodo: (id) => {},
   updateTodo: (id, todo) => {},
